8242de6695aa63155d1b4b70e29a2e955864643d
Alvin <alvin.neri.ece@gmail.com>
2020-05-24 14:10:42 +0800

    2nd

--- src/components/details.js
+++ src/components/details.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-const Details = () => {
+const Details = (props) => {
 
+    const {title,overview, vote_average, release_date, image} = props.location.state;
     return (
         <div className="margin">
+            <Link to='/' className="btn-back">Go Back</Link>
             <div className="details-container">
-                
+                <div className="details-img-container">
+                    <img src={image} alt=""/>
+                </div>
+                <div className="details-content-container">
+                    <p>Title: {title}</p>
+                    <p>Date Released: {release_date}</p>
+                    <p>Overview: {overview}</p>
+                    <p>Average Vote: {vote_average}</p>
+                </div>
             </div>
         </div>
     )
